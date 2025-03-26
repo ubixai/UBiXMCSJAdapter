c0460a39d2b3312e8610df6841847c18401fb54f
guoqiang <guoqiang.zhu@ubixai.com>
2025-03-26 18:27:27 +0800

    add CSJ 6.7.0.8.0

---
+++ UBiXMCSJAdapter/UBiXMCSJAdapter.framework/Headers/UBiXMCSJAdapter.h
@@ -0,0 +1,23 @@
+//
+//  UBiXMCSJAdapter.h
+//  UBiXMCSJAdapter
+//
+//  Created by guoqiang.zhu on 2025/3/24.
+//  Copyright © 2025 UBiX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface UBiXMCSJAdapter : NSObject
+//
+//@end
+
+//! Project version number for UBiXMCSJAdapter.
+FOUNDATION_EXPORT double UBiXMCSJAdapterVersionNumber;
+
+//! Project version string for UBiXMCSJAdapter.
+FOUNDATION_EXPORT const unsigned char UBiXMCSJAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+
